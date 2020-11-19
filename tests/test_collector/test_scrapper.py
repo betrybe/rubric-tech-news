1f8d17884e31767e7bf56969a9d3ba09f64b8f75
“brunobatista” <brunobatista101@gmail.com>
2020-11-19 13:39:28 -0300

    teste

--- tests/test_collector/test_scrapper.py
+++ tests/test_collector/test_scrapper.py
@@ -11,12 +11,3 @@ def test_validar_metodo_fetch_content_quando_status_200():
     teste = fetch_content('https://app.betrybe.com/') 
 
     assert "Aprenda a programar com uma formação de alta qualidade e só comece a pagar quando conseguir um bom trabalho." in teste
-
-# O tempo máximo de resposta do servidor deve ser configurado como parâmetro e por padrão será 3 segundos;
-def test_validar_fetch_retorna_vazio_quando_status_diferente_de_200():
-    teste = scrape(fetcher=fetch_content, pages=1) 
-    print(teste[0])
-    print(type(RESPONSE))
-    assert RESPONSE == teste[0]
-
-#Caso a resposta tenha o código de status diferente de 200, deve-se retornar uma str vazia;
